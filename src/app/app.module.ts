acf3e76bc33a665ee30bbe3f128c8ea6b109e8df
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2021-11-07 18:59:07 -0300

    Concluindo o Template;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { TemplateModule } from './template/template.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     
     
   ],
